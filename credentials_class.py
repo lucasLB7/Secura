79a04016c9bdfacd093b3fa8ef6f1cc96b92258c
Luka <plucaslambert@gmail.com>
2018-05-06 17:03:45 +0300

    Added a bit of preliminary data with expected outputs for buildup.

--- credentials_class.py
+++ credentials_class.py
@@ -1,4 +1,23 @@
+import unittest
+import readfiles
 class User_credentials():
-    def __init__(self, website , user_name_cr , password_cr)
 
-    
+    list_of_credentials = []
+
+
+    def __init__(self, website , user_name_cr , password_cr):
+        self.website = website
+        self.user_name_cr = user_name_cr
+        self.password_cr = password_cr
+
+
+
+class TestReadFiles(unittest.TestCase):
+    """
+    Class to test the  functions on the  readfiles module
+
+    Args:
+        unittest.TestCase: Class from the unittest module to create unit tests
+    """
+if __name__ == "__main__":
+    unittest.main()
