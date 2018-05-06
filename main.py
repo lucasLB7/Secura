92c13a2735f6b76f7e09977e93917ebe60f9b85e
Luka <plucaslambert@gmail.com>
2018-05-06 15:28:53 +0300

    Login WORKS! hoorah.. Ended up using a for loop with a user_validation method in our user class. Method dinply returns the list, which we transform to a list. On the main end, we have two input which we compare

--- main.py
+++ main.py
@@ -28,39 +28,32 @@ def display_users():
     '''
     return User.display_users()
 
+def valid_user():
+    return User.valid_user()
+
 def user_validation():
-    print("enter your unsername: ")
-    user_n = input()
-    print("enter your password: ")
-    pass_w = input()
-    if User.valid_user(user_n,pass_w):
-        print("ok")
-    print("not okay")
+    if valid_user():
+        print("enter your unsername: ")
+        user_n = input()
+        print("enter your password: ")
+        pass_w = input()
+        pass_list = [user_n,pass_w]
+        for user in valid_user():
+            stored_list = [user.user_name,user.password]
+            if stored_list == pass_list:
+                print("Yup")
+            else:
+                print("nop")
+                print('\n')
 
-
-# def del_user(contact):
-#     '''
-#     Function to delete a contact
-#     '''
-#     contact.delete_user()
-#
-# def find_contact(number):
-#     '''
-#     Function that finds a contact by number and returns the contact
-#     '''
-#     return Contact.find_by_name(number)
-#
-# def check_existing_contacts(number):
-#     '''
-#     Function that check if a contact exists with that number and return a Boolean
-#     '''
-#     return Contact.user_exist(number)
-#
 
 
 def main_run ():
     print("Welcome to SECURA")
-    user_validation()
+    if display_users():
+        user_validation()
+    pass
+
     while True:
         print("Use these short codes : cc - create a secure user account,\n dc - display contacts, fc -find a contact, ex -exit the contact list ")
         short_code = input().lower()
@@ -97,6 +90,11 @@ def main_run ():
                 print('\n')
                 print("You dont seem to have any contacts saved yet")
                 print('\n')
+
+        elif short_code == "li":
+            user_validation()
+
+
 
 
 
